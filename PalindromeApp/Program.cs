2b613986c433b08299d1775559a26f048f2becdc
ridwanda safatain <ridwandasafatain@gmail.com>
2024-08-06 21:47:45 +0700

    Reapply "Add project files."
    This reverts commit 5154258bf1bde64a05d60363fd13d05113e9a037.

---
+++ PalindromeApp/Program.cs
@@ -0,0 +1,45 @@
+﻿
+namespace Palindrome
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Silahkan masukkan angka : ");
+            var input = Console.ReadLine();
+            if (!int.TryParse(input, out int res)){
+                Console.WriteLine("Silahkan masukkan angka!");
+            }
+            else
+            {
+                if (IsPalindrome(input))
+                {
+                    //- true
+                    Console.WriteLine(string.Format("{0} adalah bilangan Palindrome", input));
+                }
+                else
+                {
+                    //- false
+                    Console.WriteLine(string.Format("{0} bukan bilangan Palindrome", input));
+                }
+            }            
+        }
+
+        private static bool IsPalindrome(string number)
+        {
+            var result = false;
+            var numberChar = number.ToCharArray();
+            var numberReserved = "";
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                numberReserved += numberChar[i];
+            }
+            if(number == numberReserved)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
